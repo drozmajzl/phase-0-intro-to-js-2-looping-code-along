5c2b1d9a02608e906f1cd5f0d8f2465842cd6015
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2021-12-27 23:31:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+//Assignment 1
+
+let guestList = [];
+
+function writeCards(guests, occasion) {
+    for (let i = 0; i < guests.length; i++) {
+        console.log(`Thank you, ${guests[i]}, for the wonderful ${occasion} gift!`);
+        guestList.push(`Thank you, ${guests[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return guestList;
+}
+
+guestlist(["Bob", "Jummy", "Horton"])
+
+//Assignment 2
+
+function countDown(x) {
+    while (x > -1) {
+        console.log(x);
+        x--;
+    } 
+}
